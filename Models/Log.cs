ced27404d3402a1730860d2c3ac469c9d794eaf5
Nigel <m0cvo@yahoo.co.uk>
2022-05-15 12:46:56 +0100

    Add project files.

---
+++ Models/Log.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace LogBook.Models
+{
+    public class Log
+    {
+        public int Id { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        [DataType(DataType.Time)]
+        public DateTime Time { get; set; }
+        public float Frequency { get; set; }
+        public string Mode { get; set; }
+        public int Power { get; set; }
+        public string Callsign { get; set; }
+        public int ReportSent { get; set; }
+        public int ReportReceived { get; set; }
+    }
+}
